c239583f7acb49d49d7a5f02f84f50d6c31dc48c
YippieWeb <yippie.web@gmail.com>
2024-05-21 17:33:15 -0700

    updated routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'; // for github pages
 import './App.css';
 
 import Dashboard from "./pages/Dashboard"
